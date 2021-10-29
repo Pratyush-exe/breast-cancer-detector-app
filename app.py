512953cb2d1d4ef0722cdb2b84c6ce303ef4a6fb
Pratyush K. Patnaik <78687109+Pratyush-exe@users.noreply.github.com>
2021-10-29 08:08:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def predict():
 
     # output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(file, img))
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(img))
 
 
 if __name__ == "__main__":
