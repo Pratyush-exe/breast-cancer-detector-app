e3548a7ff59346f5e183146eb45e23b923669a3b
Pratyush K. Patnaik <78687109+Pratyush-exe@users.noreply.github.com>
2021-10-29 14:40:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,34 +1,82 @@
+from __future__ import division, print_function
+# coding=utf-8
+import os
 import numpy as np
-from flask import Flask, request, jsonify, render_template
-# import pickle
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
-import cv2
+from keras.preprocessing import image
 
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
 app = Flask(__name__)
-model = load_model('model3.h5')
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/model_resnet.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+model._make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
 
 
-@app.route('/')
-def home():
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    file = request.form.items()
-    img = cv2.imread(file)
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
 
-    # final_features = [np.array(int_features)]
-    # prediction = model.predict(final_features)
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
 
-    # output = round(prediction[0], 2)
+        # Make prediction
+        preds = model_predict(file_path, model)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(img))
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        result = str(pred_class[0][0][1])               # Convert to string
+        return result
+    return None
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
