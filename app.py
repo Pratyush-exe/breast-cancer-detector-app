384a206a518d45b598625e17f3321b3569e9eccd
Pratyush K. Patnaik <78687109+Pratyush-exe@users.noreply.github.com>
2021-11-06 17:48:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,26 @@
 from __future__ import division, print_function
-# coding=utf-8
 import os
 import numpy as np
 
-# Keras
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
 from keras.preprocessing import image
 
-# Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
-# Define a flask app
 app = Flask(__name__)
-
-# Model saved with Keras model.save()
 MODEL_PATH = 'BreastCancerModels/model3.h5'
 
-# Load your trained model
 model = load_model(MODEL_PATH)
-model.make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
-
+model.make_predict_function()
 
 def model_predict(img_path, model):
     img = image.load_img(img_path, target_size=(100, 100))
 
-    # Preprocessing the image
     x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
     x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-#     x = preprocess_input(x, mode='caffe')
 
     preds = model.predict(x)
     return preds
@@ -55,24 +34,16 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
-#     if request.method == 'POST':
-        # Get the file from post request
     f = request.files['file']
 
-    # Save the file to ./uploads
     basepath = os.path.dirname(__file__)
-#     file_path = os.path.join(
-#         basepath, 'uploads', secure_filename(f.filename))
     file_path = os.path.join(
         basepath, secure_filename(f.filename))
     f.save(file_path)
 
-    # Make prediction
     preds = model_predict(file_path, model)
-    result = str(preds)              # Convert to string
+    result = str(preds)
     return result
-#     return 'working'
-#return None
 
 
 if __name__ == '__main__':
